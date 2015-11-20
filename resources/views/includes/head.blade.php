d2eabb8566cb11fdcb01842a91f870077922d2c7
ciscoo <cisco@mateo.io>
2015-11-20 15:55:46 -0600

    Starting over.

--- resources/views/includes/head.blade.php
+++
@@ -1,6 +0,0 @@
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="description" content="">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Alga-Web | @yield('title')</title>
-<link rel="stylesheet" href="assets/vendor/bootstrap.min.css">
